718b74cd73fee4998bbbcbb145af3236466d7d65
Joel Stanley <stanleyj@live.ca>
2015-09-28 18:07:45 -0300

    Removed the cache file from gedit. Set up the input file data.txt, and tried to get file reader working in Assign1.c.

--- Assignments/Assignment 1/Assign1.c
+++ Assignments/Assignment 1/Assign1.c
@@ -18,6 +18,8 @@ struct Node {
 struct Node* front = NULL;
 struct Node* end = NULL;
 
+FILE *file;
+
 void enqueue(char *userIn, char processIn, int arrivalIn, int durationIn) {
 	struct Node* new = (struct Node*)malloc(sizeof(struct Node));
 	new->user = userIn;
@@ -37,21 +39,34 @@ void enqueue(char *userIn, char processIn, int arrivalIn, int durationIn) {
 
 int main() {
 	
-	int numJobs, i;
-	printf("How many jobs will there be?\n");
-	scanf("%d", &numJobs);
-
-	for(i = 0; i < numJobs; i++) {
-		char tUser[10];
-		char tProcess;
-		int tArrival;
-		int tDuration;
-
-		printf("Input the data below seperated by a tab.\n");
-		scanf("%s	%c	%d	%d", tUser, &tProcess, &tArrival, &tDuration);
-
+	file = fopen("data", "r");
+	char line[1024];
+	memset(line, 0, 1024);
+	
+	char tUser[10];
+	char tProcess;
+	int tArrival;
+	int tDuration;
+	
+	fgets(line, 1024, file); // Skips the first line
+	
+	while(!feof(file)) {
+		sscanf(line,"%s	%c	%d	%d", tUser, &tProcess, &tArrival, &tDuration);
 		enqueue(tUser, tProcess, tArrival, tDuration);
+		printf("Added");
 	}
+	fclose(file);
+	int time = front->arrival;
+	
+	printf("\nTime	Job\n");
+	
+	while(front != NULL){
+		printf("%d	%c\n", time, front->process);
+		time += front->duration;
+		front = front->next;
+	}
+	
+	printf("%d	IDLE\n", time);
 	return 0;
 }
 
