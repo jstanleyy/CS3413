9b0583421bb586fa84b65d15b6d9265de2bcc6e5
jstanley <jstanley@unb.ca>
2015-10-14 19:13:39 -0300

    Working on Q1.

--- Assignments/Assignment 2/Q1.c
+++ Assignments/Assignment 2/Q1.c
@@ -15,6 +15,7 @@ struct Node {
 	int arrival;
 	int duration;
 	struct Node *next;
+	struct Node *prev;
 };
 
 // Used to keep track of the last job each person finished.
@@ -29,6 +30,12 @@ struct Node* end = NULL;
 
 FILE *file;
 
+struct Person info[10];
+int n;
+int curTime = 0, count = 0, k = 0;
+pthread_mutex_t m1 = PTHREAD_MUTEX_INITIALIZER;
+
+
 // Adds the Node structure to the end of the queue.
 void enqueue(char *userIn, char processIn, int arrivalIn, int durationIn) {
 	struct Node* new = (struct Node*)malloc(sizeof(struct Node));
@@ -37,21 +44,96 @@ void enqueue(char *userIn, char processIn, int arrivalIn, int durationIn) {
 	new->arrival = arrivalIn;
 	new->duration = durationIn;
 	new->next = NULL;
+	new->prev = NULL;
 
 	if(front == NULL && end == NULL) {
 		front = end = new;
 	}
 	else {
-		end->next = new;
-		end = new;
+		struct Node* check = front;
+		int i = 0;
+	 	printf("%d, %d\n", count, i);
+		for(i = 0; i <= count + 1; i++ ) {
+			printf("check: %d, %d\n", check->duration, new->duration);
+			if(new->duration == check->duration) {
+				// Add 1 behind
+				if(check == end) {
+					check->next = new;
+					new->prev = check;
+				}
+				else {
+					struct Node* temp = check->next;
+					printf("here2");
+					check->next = new;
+					new->next = temp;
+					new->prev = check;
+					temp->prev = new;
+					printf("here2");
+					break;
+				}
+			}
+			else if (new->duration < check->duration) {
+				// Add 1 before
+				if(check == front) {
+					new->next = front;
+					front->prev = new;
+					front = new;
+				}
+				else {
+					struct Node* temp = check->prev;
+					temp->next = new;
+					new->next = check;
+					new->prev = check->prev;
+					check->prev = new;
+				}
+				break;
+			}
+			else if (check->next == NULL) {
+				// Add to end
+				check->next = new;
+				new->prev = check;
+				end = new;
+			}
+			else {	
+				check = check->next;
+			}
+		}
+//		end->next = new;
+//		end = new;
 	}
 }
  
-void run() {
-	sleep(front->duration);
+void * run(void *in) {
+	int core = *((int *) in);
+	int timeInCore = curTime;
+	for( ; ; ) {
+		if (front != end) {
+		pthread_mutex_lock(&m1);
+		if(timeInCore >= front->arrival) {
+			printf("%d	%c\n", timeInCore, front->process);
+			int sleepTime = front->duration;
+			k++;
+			pthread_mutex_unlock(&m1);
+			//Keeps track of the last job for each user.
+			int j;
+			for(j = 0; j < count; j++) {
+				if(strcmp(info[j].name, front->user) == 0) {
+					info[j].last = timeInCore + front->duration;
+					break;
+				}
+			}
+			front = front->next;
+			
+     			sleep(sleepTime);
+			timeInCore += sleepTime;
+		}
+		else {
+			timeInCore++;
+		}
+		}
+	}
+	printf("%d	CPU %d IDLE\n", timeInCore, core);
 }
-
-struct Person info[10];
 
 int main(int argc, char *argv[]) {
 	
@@ -64,17 +146,18 @@ int main(int argc, char *argv[]) {
 	char tProcess;
 	int tArrival;
 	int tDuration;
-	int count = 0, done = 0;
+	int done = 0;
 	int i, skip = 0, j, dup = 0;
-	int n = *argv[1] - 48;
+	n = *argv[1] - 48;
 
 	fgets(line, 1024, file); // Skips the first line of the input.
 	
 	while(done == 0) {
 		char *tUser = (char *)malloc(sizeof(char[20])); // Allocates memory to be used for the input names
 		if(fscanf(file,"%s	%c	%d	%d", tUser, &tProcess, &tArrival, &tDuration) == 4) {
+			printf("adding user %s\n", tUser);
 			enqueue(tUser, tProcess, tArrival, tDuration);
-		
+			printf("added %s\n", tUser);	
 			// Checks if the name already exists in the array, and skips if so.
 			for(i = 0; i < count; i++) {
 				if(strcmp(info[i].name, tUser) == 0) { 
@@ -99,19 +182,29 @@ int main(int argc, char *argv[]) {
 	
 	fclose(file); // Closes the file as it is no longer needed.
 
-	int time = front->arrival;
+	curTime = 0;
 
 	printf("Time	Job\n");
 	
-	int k = 0;
 	int timeDone[n]; // Used to keep track of cores.
 
 	for(i = 0; i < n; i++) {
 		timeDone[i] = 0;
 	}
 	
+	pthread_t thread_id[n];
+	for(i = 0; i < n; i++) {
+		int *coreNum = malloc(sizeof(*coreNum));
+		*coreNum = i + 1;
+		pthread_create(&thread_id[i], NULL, &run, coreNum);
+	}
+	
+	for(i = 0; i< n; i++) {
+    	pthread_join(thread_id[i], NULL);
+	}
+
 	// Prints each process that is executing on each CPU.
-	while(k < count) {
+	/*while(k < count) {
 		for(i = 0; i < n; i++) { // Loops for each CPU.
 			if(time >= front->arrival) { // Can only proceed if the next task has arrived.
 				if(time >= timeDone[i]) {
@@ -143,12 +236,12 @@ int main(int argc, char *argv[]) {
 
 	time++;
 
-	}
+	} */
 		
 	// Prints the Idle times for each CPU.
-	for(i = 0; i < n; i++) {
-		printf("%d	CPU %d IDLE\n", timeDone[i], i + 1);
-	}
+	//for(i = 0; i < n; i++) {
+		//printf("%d	CPU %d IDLE\n", curTime, i + 1);
+	//}
 	
 	// Prints the Summary.
 	printf("\nSummary\n");
