ce6ba28f7ae5e784dc3305d390b6b29d14b37d4e
jstanley <jstanley@unb.ca>
2015-10-31 12:53:03 -0300

    Finished Q1.

--- Assignments/Assignment 3/Q1.c
+++ Assignments/Assignment 3/Q1.c
@@ -15,39 +15,36 @@ sem_t barbReady, custReady, seats, done;
 
 
 void* barber() {
-
 	for( ; ; ) { // Infinite loop
 		sem_wait(&custReady); // Waits for a customer.
 		sem_wait(&seats); // Waits for access to change the number of available seats.
 		numOfChairs += 1; // A chair becomes available.
-		//sem_post(&barbReady);
 		sem_post(&seats);
-		sleep(10);
-		printf("I cut a customers hair.\n");
 		sem_post(&barbReady);
+		// Cutting hair here.
 	}
 }
 
 void* customer() {
-
-	printf("A new customer arrived.\n");
-	wait(&seats);
-	if(numOfChairs > 0) {
-		numOfChairs -= 1;
-		sem_post(&custReady);
-		sem_post(&seats);
-		sem_wait(&barbReady);
-		printf("I had my hair cut.\n");
-	}
-	else {
-		sem_post(&seats);
-		printf("No seats, I'm leaving.\n");
+	for( ; ; ) { // Infinite loop
+		
+		wait(&seats);
+		if(numOfChairs > 0) {
+			numOfChairs -= 1;
+			sem_post(&custReady);
+			sem_post(&seats);
+			sem_wait(&barbReady);
+			// Customer is getting a haircut here.
+		}	
+		else {	
+			sem_post(&seats); // Customer is leaving
+		}
 	}
 }
 
 int main(int argc, char *argv[]) {
 
-	numOfChairs	= atoi(argv[1]);
+	numOfChairs = atoi(argv[1]);
 	printf("Number of chairs is: %d\n", numOfChairs);
 
 	sem_init(&barbReady, 0, 0);
@@ -55,13 +52,12 @@ int main(int argc, char *argv[]) {
 	sem_init(&seats, 0, 1);
 	sem_init(&done, 0, 0);
 	
-	pthread_t barbThread;
+	pthread_t barbThread, custThread;
 
 	pthread_create(&barbThread, NULL, &barber, NULL);
+	pthread_create(&custThread, NULL, &customer, NULL);
 	printf("Thread created.\n");
 
-	for ( ; ; ) {
-		customer();
-		sleep(2);
-	}
+	pthread_join(barbThread, NULL);
+	pthread_join(custThread, NULL);
 }
